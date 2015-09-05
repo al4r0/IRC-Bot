c7647eac5e752828fc5b8321a26901b8b8321a21
Álvaro Ramos <v4char@gmail.com>
2015-09-06 01:18:43 +0200

    Fix Bug's

--- irc_bot.py
+++ irc_bot.py
@@ -7,7 +7,7 @@ canal = "#canal"
 nombre = "v4charbot"
 puerto = 6667
 
-def respuesta_ping(ircmsg,canal):
+def respuesta_ping(ircmsg, canal):
       if ircmsg.find("PING :") != -1:
             respuesta_ping = ircmsg
             respuesta_ping = respuesta_ping.replace("I", "O")
@@ -22,18 +22,18 @@ def unirse_a_canal(canal):
       irc.send("JOIN "+ canal +"\n\r")
 
 def  obtener_nick(canal, ircmsg):
-      if(ircmsg.find("PRIVMSG "+canal) != -1):
+      if ircmsg.find("PRIVMSG "+canal) != -1:
             nick = ircmsg.split('!', 1 )
             nick = nick[0].replace(":", "",1)
             return nick
 
 def obtener_mensaje(canal, ircmsg):
-      if(ircmsg.find("PRIVMSG "+canal) != -1):
+      if ircmsg.find("PRIVMSG "+canal) != -1:
             mensaje = ircmsg.split(canal+' ', 1 )
             mensaje = mensaje[1].replace(":", "",1)
             return mensaje
 
-def mostrar_chat(canal, ircmsg):
+def mostrar_chat(ircmsg, canal):
       print (obtener_nick(canal, ircmsg)+": "+obtener_mensaje(canal, ircmsg))
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,9 +49,11 @@ unirse_a_canal(canal)
 while 1:
       ircmsg = irc.recv(1024)
       ircmsg = ircmsg.strip('\n\r')
-    
+      
+      if ircmsg.find("PRIVMSG "+canal) != -1:
+            mostrar_chat(ircmsg, canal)
+            
       respuesta_ping(ircmsg,canal)
-      mostrar_chat(canal, ircmsg)
-    
+      
       if ircmsg.find("palabra clave") != -1:
             enviar_mensaje(canal, "No digas la palabra clave")

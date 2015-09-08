10e2c4b851c729d1977debeb81cc48b130f58548
Álvaro Ramos <v4char@gmail.com>
2015-09-08 04:23:27 +0200

    Update irc_bot.py

--- irc_bot.py
+++ irc_bot.py
@@ -2,8 +2,8 @@
 import socket
 import time
 
-servidor = "irc.ircnode.com"
-canal = "#bottest"
+servidor = "servidor_irc"
+canal = "#canal"
 nombre = "v4charbot"
 puerto = 6667
 
@@ -22,6 +22,7 @@ def respuesta_ping(ircmsg, canal):
             respuesta_ping = entre(respuesta_ping, "PING :","\r\n:")
             respuesta_ping = "PONG :"+respuesta_ping+"\r\n"
             irc.send(respuesta_ping)
+            print respuesta_ping
             unirse_a_canal(canal)
         
 def enviar_mensaje(canal , msg):
@@ -29,16 +30,17 @@ def enviar_mensaje(canal , msg):
     
 def unirse_a_canal(canal):
       irc.send("JOIN "+ canal +"\n\r")
-
+      ########Revisar esto muy importante#######
+"""
 def  obtener_nick(canal, ircmsg):
-      if ircmsg.find("PRIVMSG "+canal) != -1:
+      if ircmsg.find("PRIVMSG ") != -1:
             nick = ircmsg.split('!', 1 )
             nick = nick[0].replace(":", "",1)
             print nick
             return nick
 
 def obtener_mensaje(canal, ircmsg):
-      if ircmsg.find("PRIVMSG "+canal) != -1:
+      if ircmsg.find("PRIVMSG ") != -1:
             mensaje = ircmsg.split(canal+' ', 1 )
             mensaje = mensaje[1].replace(":", "",1)
             print mensaje
@@ -46,7 +48,7 @@ def obtener_mensaje(canal, ircmsg):
 
 def mostrar_chat(ircmsg, canal):
       print (obtener_nick(canal, ircmsg)+": "+obtener_mensaje(canal, ircmsg))
-
+"""
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((servidor, puerto))
 irc.send("USER "+ nombre +" "+ nombre +" "+ nombre +" :Bot by v4char\n\r")
@@ -60,9 +62,7 @@ unirse_a_canal(canal)
 while 1:
       ircmsg = irc.recv(512)
       ircmsg = ircmsg.strip('\n\r')
-      
-      obtener_nick(canal, ircmsg)
-      obtener_mensaje(canal, ircmsg)
+
             
       if ((ircmsg.find("PRIVMSG") != -1) != 1):
             respuesta_ping(ircmsg,canal)
